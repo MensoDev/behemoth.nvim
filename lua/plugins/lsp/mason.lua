8ec0dc03556a756ac05b6e3f55722a5cc6f300a0
Emerson Trindade (menso.dev.br) <menso@menso.dev.br>
2025-12-08 17:57:09 -0300

    ✨ :: add lsp + cmp for roslyn

---
+++ lua/plugins/lsp/mason.lua
@@ -0,0 +1,16 @@
+return {
+    "mason-org/mason.nvim",
+    opts = {
+        registries = {
+            "github:mason-org/mason-registry",
+            "github:Crashdummyy/mason-registry",
+        },
+        ui = {
+            icons = {
+                package_installed = "✓",
+                package_pending = "➜",
+                package_uninstalled = "✗",
+            },
+        },
+    }
+}
