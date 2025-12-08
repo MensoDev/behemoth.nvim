ef4451d13f211a8f4017eb4aac84779831d5219f
Emerson Trindade (menso.dev.br) <menso@menso.dev.br>
2025-12-08 17:11:15 -0300

    fix :: improvements on treesitter settings

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,44 +1,21 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        event = { "BufReadPre", "BufNewFile" },
-        build = ":TSUpdade",
-        config = function()
-            local treesitter = require("nvim-treesitter.configs")
+    "nvim-treesitter/nvim-treesitter",
+    branch = 'master',
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+        require'nvim-treesitter.configs'.setup {
+            ensure_installed = { 
+                "lua",
+            },
 
-            treesitter.setup({
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                },
-                indent = { enable = true },
-                ensure_installed = {
-                    "bash",
-                    "c",
-                    "c_sharp",
-                    "cpp",
-                    "css",
-                    "csv",
-                    "dart",
-                    "dockerfile",
-                    "fish",
-                    "git_rebase",
-                    "html",
-                    "http",
-                    "hyprlang",
-                    "javascript",
-                    "json",
-                    "lua",
-                    "razor",
-                    "rust",
-                    "scss",
-                    "sql",
-                    "typescript",
-                    "vim",
-                    "xml",
-                    "yaml",
-                }
-            })
-        end
-    }
+            sync_install = false,
+            auto_install = true,
+
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+        }
+    end
 }
