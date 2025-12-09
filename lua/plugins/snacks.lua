f4e3ca9cb6dde5f91f96515c5708812d37b32084
Emerson Trindade (menso.dev.br) <menso@menso.dev.br>
2025-12-09 11:18:19 -0300

    ✨ :: add more snacks modules and keymaps

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -12,23 +12,27 @@ return {
                 replace_netrw = true, -- Replace netrw with the snacks explorer
                 -- trash = true, -- Use the system trash when deleting files
             },
+            lazygit = { enabled = true },
         },
         keys = {    
             -- Top Pickers & Explorer
             { "<leader>e", function() Snacks.explorer() end, desc = "file explorer" },
 
             { "<leader><space>", function() Snacks.picker.buffers() end, desc = "find buffers" },
+            { "<leader>ff", function() Snacks.picker.files() end, desc = "find files" },
             { "<leader>fg", function() Snacks.picker.grep() end, desc = "find files with grep" },
             { "<leader>fs", function() Snacks.picker.smart() end, desc = "find files - smart" },
-
+            { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "find config files" },
+            
             -- Github
-            { "<leader>gi", function() Snacks.picker.gh_issue() end, desc = "GitHub Issues (open)" },
-            { "<leader>gI", function() Snacks.picker.gh_issue({ state = "all" }) end, desc = "GitHub Issues (all)" },
-            { "<leader>gp", function() Snacks.picker.gh_pr() end, desc = "GitHub Pull Requests (open)" },
-            { "<leader>gP", function() Snacks.picker.gh_pr({ state = "all" }) end, desc = "GitHub Pull Requests (all)" },
-
+            { "<leader>gi", function() Snacks.picker.gh_issue() end, desc = "gitHub issues (open)" },
+            { "<leader>gI", function() Snacks.picker.gh_issue({ state = "all" }) end, desc = "gitHub issues (all)" },
+            { "<leader>gp", function() Snacks.picker.gh_pr() end, desc = "gitHub pull requests (open)" },
+            { "<leader>gP", function() Snacks.picker.gh_pr({ state = "all" }) end, desc = "gitHub pull requests (all)" },
+            { "<leader>gl", function() Snacks.lazygit() end, desc = "open lazygit" },
+           
             -- Theme Pickers
-            { "<leader>th", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
+            { "<leader>th", function() Snacks.picker.colorschemes() end, desc = "show colorschemes" },
         }
     },
 }
