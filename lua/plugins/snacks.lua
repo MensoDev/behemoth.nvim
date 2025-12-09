98c12f0d2851f53f67bb7bed0484f9337ead016c
Emerson Trindade (menso.dev.br) <menso@menso.dev.br>
2025-12-09 17:17:28 -0300

    ✨ :: add more keymaps for snacks |

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,12 +7,14 @@ return {
         ---@type snacks.Config
         opts = {
             dashboard = { enabled = true },
+            picker = { enabled = true },
             explorer = { 
                 enabled = true,
                 replace_netrw = true, -- Replace netrw with the snacks explorer
                 -- trash = true, -- Use the system trash when deleting files
             },
             lazygit = { enabled = true },
+            gh = { enabled = true },
         },
         keys = {    
             -- Top Pickers & Explorer
@@ -23,16 +25,29 @@ return {
             { "<leader>fg", function() Snacks.picker.grep() end, desc = "find files with grep" },
             { "<leader>fs", function() Snacks.picker.smart() end, desc = "find files - smart" },
             { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "find config files" },
-            
+
+            -- LSP
+            { "gd", function() Snacks.picker.lsp_definitions() end, desc = "goto definition" },
+            { "gD", function() Snacks.picker.lsp_declarations() end, desc = "goto declaration" },
+            { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "show references" },
+            { "gI", function() Snacks.picker.lsp_implementations() end, desc = "goto implementation" },
+            { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "goto t[y]pe definition" },
+            { "gai", function() Snacks.picker.lsp_incoming_calls() end, desc = "c[a]lls incoming" },
+            { "gao", function() Snacks.picker.lsp_outgoing_calls() end, desc = "c[a]lls outgoing" },
+            { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP symbols" },
+            { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP workspace symbols" },           
+
             -- Github
             { "<leader>gi", function() Snacks.picker.gh_issue() end, desc = "gitHub issues (open)" },
             { "<leader>gI", function() Snacks.picker.gh_issue({ state = "all" }) end, desc = "gitHub issues (all)" },
             { "<leader>gp", function() Snacks.picker.gh_pr() end, desc = "gitHub pull requests (open)" },
             { "<leader>gP", function() Snacks.picker.gh_pr({ state = "all" }) end, desc = "gitHub pull requests (all)" },
+            { "<leader>gn", function() Snacks.gh.pr() end, desc = "gitHub pull requests (all)" },
+
             { "<leader>gl", function() Snacks.lazygit() end, desc = "open lazygit" },
            
             -- Theme Pickers
             { "<leader>th", function() Snacks.picker.colorschemes() end, desc = "show colorschemes" },
-        }
+        },
     },
 }
