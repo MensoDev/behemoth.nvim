0a1b16ca5a6f4a0e1eac2dc3b20f7cd630182b17
Emerson Trindade (menso.dev.br) <menso@menso.dev.br>
2026-05-21 13:45:24 -0300

    clean all | reboot from zero | see you later

--- lua/core/autocmds.lua
+++
@@ -1,9 +0,0 @@
--- Behemoth | Autocmds
---
-
--- Highlight when yanking
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  callback = function() vim.hl.hl_op() end,
-})
---
